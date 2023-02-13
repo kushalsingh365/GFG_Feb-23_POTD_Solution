c57aa93f58e1a91f45ad3b6dc655d5e03e93ade4
Kushal Singh <81277166+kushalsingh365@users.noreply.github.com>
2023-02-13 21:06:49 +0530

    12_Feb_23

---
+++ 12) Balloon Everywhere.cpp
@@ -0,0 +1,16 @@
+//User function Template for C++
+
+class Solution{
+public:
+    int maxInstance(string s){
+        int arr[26]={0};
+        
+        for(int i=0;i<s.length();i++){
+            arr[s[i]-'a']++;
+        }
+        int ans = min({arr[0],arr[1],arr[11]/2,arr[14]/2,arr[13]});
+        return ans;
+        
+    }
+   // }
+};
